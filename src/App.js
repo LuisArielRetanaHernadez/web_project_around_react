1c8bc460cd465a20705554a3ff23d638490e3c7c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-25 15:03:02 -0600

    pasando el popup create card arriba del popup editar perfil

--- src/App.js
+++ src/App.js
@@ -40,7 +40,49 @@ function App() {
       <Footer />
       {/* <!-- Popup to open the create card form --> */}
 
+      <PopupWithForm title="Agregar una nueva tarjeta" selector=".popup--create-card" isOpen={isAddPlacePopupOpen} >
+        <form className="form popup__form" action="">
+          <div className="form__field-component">
+            <input
+              type="text"
+              id="input-profile-name"
+              className="form__input popup__form-input"
+              name="title"
+              required
+              minlength="2"
+              maxlength="30"
+              placeholder="title"
+            />
+            <span
+              className="form__error-message"
+              id="input-profile-name-error"
+            ></span>
+          </div>
 
+          <div className="form__field-component">
+            <input
+              className="form__input popup__form-input"
+              id="input-profile-url-image"
+              name="url"
+              required
+              placeholder="url"
+              type="url"
+            />
+            <span
+              className="form__error-message"
+              id="input-profile-url-image-error"
+            ></span>
+          </div>
+
+          <button
+            className="button form__button-submit popup__button-submit"
+            id="button-update-profile"
+            type="submit"
+          >
+            Guardar
+          </button>
+        </form>
+      </PopupWithForm>
       {/* <!-- Popup to open the update profile form --> */}
 
       <PopupWithForm title="Actualizar tu perfil" selector=".popup--update-profile" isOpen={isEditProfilePopupOpen} >
@@ -86,49 +128,7 @@ function App() {
         </form>
       </PopupWithForm>
 
-      <PopupWithForm title="Agregar una nueva tarjeta" selector=".popup--create-card" isOpen={isAddPlacePopupOpen} >
-        <form className="form popup__form" action="">
-          <div className="form__field-component">
-            <input
-              type="text"
-              id="input-profile-name"
-              className="form__input popup__form-input"
-              name="title"
-              required
-              minlength="2"
-              maxlength="30"
-              placeholder="title"
-            />
-            <span
-              className="form__error-message"
-              id="input-profile-name-error"
-            ></span>
-          </div>
 
-          <div className="form__field-component">
-            <input
-              className="form__input popup__form-input"
-              id="input-profile-url-image"
-              name="url"
-              required
-              placeholder="url"
-              type="url"
-            />
-            <span
-              className="form__error-message"
-              id="input-profile-url-image-error"
-            ></span>
-          </div>
-
-          <button
-            className="button form__button-submit popup__button-submit"
-            id="button-update-profile"
-            type="submit"
-          >
-            Guardar
-          </button>
-        </form>
-      </PopupWithForm>
       {/* <!-- Popup to open the imagen of selected card --> */}
 
       {/* <!-- popup update image user me --> */}
