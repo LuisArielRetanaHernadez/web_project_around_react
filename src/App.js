4111deab81d71bd4c745b40f0d517efd0c7be8a9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-27 19:53:58 -0600

    creando el popup para actulizar el avatar del user current

--- src/App.js
+++ src/App.js
@@ -143,9 +143,37 @@ function App() {
       </PopupWithForm>
 
 
+      {/* <!-- popup update image user me --> */}
+
+      <PopupWithForm title="Actualizar tu perfil" selector=".popup--upload-avatar-user-me" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+        <form className="form popup__form" action="">
+          <div className="form__field-component">
+            <input
+              className="form__input popup__form-input"
+              id="input-profile-url-image"
+              name="avatar"
+              type="url"
+              required
+            />
+            <span
+              className="form__error-message"
+              id="input-profile-url-image-error"
+            ></span>
+          </div>
+
+          <button
+            className="button form__button-submit popup__button-submit"
+            id="button-update-profile"
+            type="submit"
+          >
+            Guardar
+          </button>
+        </form>
+      </PopupWithForm>
+
       {/* <!-- Popup to open the imagen of selected card --> */}
 
-      {/* <!-- popup update image user me --> */}
+
       {selectedCard?.url && <ImagePopup selectedCard={selectedCard} onClose={closeAllPopups} />}
       {/* <!-- popup confirm delete card --> */}
 
