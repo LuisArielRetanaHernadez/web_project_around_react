305886170ba66e7eeeee39f814de364a743585bf
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-27 19:18:30 -0600

    add api.js

---
+++ src/utils/api.js
@@ -0,0 +1,144 @@
+// constat
+import {
+  TOKEN,
+  URL_BASE,
+  GroupId
+} from '../contast/contast';
+
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  async getInitialCards() {
+    const res = await fetch(`${this._baseUrl}/cards`, {
+      method: 'GET',
+      headers: this._headers
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async createCard(name, link) {
+    const res = await fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async deleteCard(cardId) {
+    const res = await fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    });
+
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  // likes card
+
+  async likeCard(cardId) {
+    const res = await fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this._headers
+    });
+
+    if (res.ok) {
+      return res.json();
+    }
+
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async likeCardDelete(cardId) {
+    const res = await fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    });
+
+    if (res.ok) {
+      return res.json();
+    }
+
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  // users
+
+  async getUserInfo() {
+    const res = await fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async getUserAvatar(avatar) {
+    const res = await fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatar
+      })
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async updateUserInfo(name, about) {
+    const res = await fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: name,
+        about: about
+      })
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+
+  async updateAvatarUser(avatar) {
+    const res = await fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatar
+      })
+    });
+    if (res.ok) {
+      return res.json();
+    }
+    return await Promise.reject(`Error: ${res.status}`);
+  }
+}
+
+const api = new Api({
+  baseUrl: `${URL_BASE}/v1/${GroupId}`,
+  headers: {
+    authorization: TOKEN,
+    "Content-Type": "application/json"
+  }
+});
+
+export default api
